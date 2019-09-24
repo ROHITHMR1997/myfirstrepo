ce25611f6dbebe80fd57cdbd97c5a2dd00e28d52
dev13 <dev13@dev13-desktop>
2019-09-24 14:53:18 +0530

    initial commit by rohith

---
+++ SalesToolV2019/src/com/ust/beans/Main.java
@@ -0,0 +1,15 @@
+package com.ust.beans;
+
+public class Main {
+	public static void main(String [] args) {
+		SalesData objSalesData = new SalesData();
+		System.out.println("Welcome to sales app");
+		displayGreeting();
+		objSalesData.display();
+		
+	}
+	private static void displayGreeting() {
+		
+	}
+
+}
